c3f19d2d20520b7f1bb8ad1c1cb2d1846c4dc173
nage <voronin@vezetvsem.ru>
2018-07-14 12:58:44 +0300

    try to fix deploy on heroku

--- server/index.js
+++ server/index.js
@@ -8,11 +8,10 @@ const { sequelize } = require('./models/');
 
 const app = express();
 const staticPath = process.env.NODE_ENV === 'prod' ? '../build' : 'public';
-const HOSTNAME = process.env.HOST || 'localhost';
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/graphql', graphqlRoutes);
 app.use(express.static(path.join(__dirname, staticPath)));
 
-app.listen(3000, () => console.log(`Express app listening on ${HOSTNAME}:${PORT}`));
+app.listen(3000, () => console.log(`Express app listening on port ${PORT}`));
